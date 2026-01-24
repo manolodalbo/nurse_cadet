9b5f59009c9543e6946c630bab6877f079be4648
manolodalbo <manuel_dal_bo@brown.edu>
2026-01-24 13:41:46 -0500

    adding 1000

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from save import save_data
 
 def main():
     print("processing data")
-    # base_folder = "D:/census/JPGandTIFF/108598756"
-    base_folder = "data"
+    base_folder = "D:/census/JPGandTIFF/108598756"
+    # base_folder = "data"
     extracted_data = process(base_folder)
     save_data(extracted_data)
 
